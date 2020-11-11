6d5d3f6527b84efd44ce07746cac8e3d8c230de3
MarcosApSousa <marcos.sousafilho04@gmail.com>
2020-11-11 10:56:20 -0300

    Criação das classes de Endereço e Cliente

---
+++ src/main/java/br/edu/iftm/os/model/Endereco.java
@@ -0,0 +1,19 @@
+
+package br.edu.iftm.os.model;
+
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Endereco implements Serializable {
+        private int id;
+        private String descricao;
+        private String logradouro;
+        private String numero;
+        private String complemento;
+        private String bairro;
+        private String cep;
+        private String cidade;
+        private String estado;
+}
